4caacb6f91eac8e2efa69790d0f7ef21a37098cd
Yordan Reyes <reyes.yordan2020@gmail.com>
2025-04-27 19:38:11 -0400

    Update final clase 2

--- clase2/app.py
+++ clase2/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+class Pelicula: 
+    def __init__(self, nombre, año, protagonista):
+        self.nombre = nombre
+        self.año = año
+        self.protagonista = protagonista
+        
 @app.route("/estructura")
 def estructura_datos():
     peliculas = [
@@ -8,7 +14,16 @@ def estructura_datos():
         "Harry Potter",
         "Volver al futuro"
     ]
-    return render_template("estructuras.html",movies=peliculas)
+
+    lobo = {
+        "Nombre" : "El lobo de wall street",
+        "Año": "2013",
+        "Protagonista" : "Leonardo DiCaprio"
+    }
+
+    volver = Pelicula(nombre="Volver al futuro", año="1985", protagonista="Michael J. Fox")
+
+    return render_template("estructuras.html",movies=peliculas, destacada = lobo, favorita = volver)
 
 
 if __name__=="__main__":
