857f02e04a2f64ad9e1f0b6205e7f1b7a1cae0f0
Yordan Reyes <reyes.yordan2020@gmail.com>
2025-04-30 11:13:26 -0400

    E03 condicionales
    ¿Cómo usar condicionales en Flask de Python? | Curso de Flask | E03

--- clase3/app.py
+++ clase3/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+"""¿Cómo usar condicionales en Flask de Python? | Curso de Flask | E03"""
 @app.route("/condicionales")
 def condicional():
     return render_template("condicional.html", equipo = "Inter")
