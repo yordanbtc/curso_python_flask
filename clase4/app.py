f87d607379c522a9799ac581cfa58415f54a3709
Yordan Reyes <reyes.yordan2020@gmail.com>
2025-04-29 22:50:59 -0400

    upload clase 04

--- clase4/app.py
+++ clase4/app.py
@@ -15,8 +15,19 @@ def loop_for():
             "MANCHESTER UNITED",
             "OPORTO"
             ]
+    equipos_cham = {
+        "REAL MADRID": 14,
+        "MILAN AC": 7,
+        "LIVERPOOOL": 6,
+        "BAYER MUNICH": 5,
+        "BARCELONA" : 5,
+        "AJAX AMSTERD": 4,
+        "INTER MILAN": 3,
+        "MANCHESTER UNITED" : 3
+
+    }
     
-    return render_template("for_loop.html", teams = equipos)
+    return render_template("for_loop.html", teams = equipos, teams_dic = equipos_cham)
 
 if __name__=="__main__":
     app.run()
