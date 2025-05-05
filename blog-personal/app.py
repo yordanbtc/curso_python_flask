c89a01ba1ff855926cb127f48c77c28777284bbe
Yordan Reyes <reyes.yordan2020@gmail.com>
2025-05-04 23:16:52 -0400

    final clase 07
    creación de blog

--- blog-personal/app.py
+++ blog-personal/app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect, url_for
+import datetime
+from pymongo import MongoClient
 
 app = Flask(__name__)
+cliente = MongoClient("mongodb+srv://yordanbtc:2daDnmDE2F4gSNfG@tutorial.wxxqpk9.mongodb.net/")
+app.db = cliente.blog
 
-@app.route("/")
 
-def blo_personal():
-    return render_template("index.html")
+entradas=[entrada for entrada in app.db.contenido.find({})]
+#print (entradas)
+
+@app.route("/", methods =["GET", "POST"])
+
+def home():
+    if request.method == "POST":
+        titulo = request.form.get("tit")
+        entry = request.form.get("content")
+        fecha_formato =datetime.datetime.today().strftime("%d-%m-%Y")
+        parametros = {"titulo":titulo, "contenido": entry, "fecha": fecha_formato}
+        entradas.append(parametros)
+        app.db.contenido.insert_one(parametros)
+        return redirect(url_for('home'))
+
+
+    return render_template("index.html", entradas=entradas)
 
 if __name__=="__main__":
     app.run()
